aeb578bb3c0538d3729a6977571d55508821ea82
Benjin Yap <devnull@localhost>
2014-04-16 13:47:12 -0400

    initial.

---
+++ AnyGameEngineEditor/MainWindow.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AnyGameEngineEditor {
+	partial class MainWindow {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose (bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose ();
+			}
+			base.Dispose (disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent () {
+			this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openGameItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveGameItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mainMenuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// mainMenuStrip
+			// 
+			this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+			this.mainMenuStrip.Name = "mainMenuStrip";
+			this.mainMenuStrip.Size = new System.Drawing.Size(627, 28);
+			this.mainMenuStrip.TabIndex = 1;
+			this.mainMenuStrip.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openGameItem,
+            this.saveGameItem,
+            this.saveAsToolStripMenuItem,
+            this.exitItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+			this.newToolStripMenuItem.Text = "New";
+			// 
+			// openGameItem
+			// 
+			this.openGameItem.Name = "openGameItem";
+			this.openGameItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openGameItem.Size = new System.Drawing.Size(175, 24);
+			this.openGameItem.Text = "Open";
+			this.openGameItem.Click += new System.EventHandler(this.onOpenGameClick);
+			// 
+			// saveGameItem
+			// 
+			this.saveGameItem.Name = "saveGameItem";
+			this.saveGameItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveGameItem.Size = new System.Drawing.Size(175, 24);
+			this.saveGameItem.Text = "Save";
+			this.saveGameItem.Click += new System.EventHandler(this.onSaveGameClick);
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+			this.saveAsToolStripMenuItem.Text = "Save As";
+			// 
+			// exitItem
+			// 
+			this.exitItem.Name = "exitItem";
+			this.exitItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+			this.exitItem.Size = new System.Drawing.Size(175, 24);
+			this.exitItem.Text = "Exit";
+			this.exitItem.Click += new System.EventHandler(this.onExitClick);
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(627, 324);
+			this.Controls.Add(this.mainMenuStrip);
+			this.Name = "MainWindow";
+			this.Text = "Form1";
+			this.mainMenuStrip.ResumeLayout(false);
+			this.mainMenuStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip mainMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openGameItem;
+		private System.Windows.Forms.ToolStripMenuItem saveGameItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitItem;
+	}
+}
+
---
+++ AnyGameEngineEditor/MainWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AnyGameEngine;
+using AnyGameEngine.LogicItems;
+
+namespace AnyGameEngineEditor {
+	public partial class MainWindow : Form {
+		public Game Game;
+
+		public MainWindow () {
+			InitializeComponent ();
+			LoadGame (@"C:\Users\Benjin\Desktop\Bitbucket\AnyGameEngineEditor\AnyGameEngineEditor\bin\Debug\Games\Pokemon test\test.xml");
+			
+		}
+
+		private void onOpenGameClick (object sender, EventArgs e) {
+
+		}
+
+		private void onSaveGameClick (object sender, EventArgs e) {
+
+		}
+
+		private void onExitClick (object sender, EventArgs e) {
+			Application.Exit ();
+		}
+
+		private bool LoadGame (string path) {
+			Game = new Game ();
+			
+			if (Game.Load (path)) {
+				/*foreach (TabPage page in this.mainTabControl.TabPages) {
+					if (page.Controls.Count > 0) {
+						((MainTabPanel) page.Controls [0]).UpdatePanel ();
+					}
+				}*/
+
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+}
