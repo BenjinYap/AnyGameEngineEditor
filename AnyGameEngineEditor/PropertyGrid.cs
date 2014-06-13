58d7f708bb964616b3f1fdc05be2e435c79da2f5
Benjin Yap <benjyay.dev@gmail.com>
2014-06-12 21:36:51 -0400

    moved propertygrid description from a separate panel into the actual cells.

--- AnyGameEngineEditor/PropertyGrid.cs
+++ AnyGameEngineEditor/PropertyGrid.cs
@@ -2,53 +2,56 @@
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace AnyGameEngineEditor {
-	public class PropertyGrid:SplitContainer {
-		private Dictionary <string, Action> keys = new Dictionary<string,Action> ();
-		private DataGridView grid = new DataGridView ();
-		private Label description = new Label ();
+	public class PropertyGrid:DataGridView {
+		private List <Row> rows = new List<Row> ();
 
 		public PropertyGrid () {
-			this.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			grid.Dock = DockStyle.Fill;
-			grid.RowHeadersVisible = false;
-			grid.ColumnCount = 2;
-			grid.Columns [0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			grid.Columns [1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			grid.Columns [0].HeaderText = "Property";
-			grid.Columns [1].HeaderText = "Value";
-			grid.ReadOnly = true;
-			grid.AllowUserToAddRows = false;
-			grid.AllowUserToDeleteRows = false;
-			grid.CellDoubleClick += onCellDoubleClick;
-			grid.CurrentCellChanged += onCurrentCellChanged;
-			grid.KeyDown += onKeyDown;
-			this.Panel1.Controls.Add (grid);
-
-			description.Dock = DockStyle.Fill;
-			this.Panel2.Controls.Add (description);
-
+			this.Dock = DockStyle.Fill;
+			this.RowHeadersVisible = false;
+			this.ColumnCount = 2;
+			this.Columns [0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+			this.Columns [1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+			this.Columns [0].HeaderText = "Property";
+			this.Columns [1].HeaderText = "Value";
+			this.ReadOnly = true;
+			this.AllowUserToAddRows = false;
+			this.AllowUserToDeleteRows = false;
+			this.CellDoubleClick += onCellDoubleClick;
+			this.KeyDown += onKeyDown;
 			this.Disposed += onDisposed;
+			this.CurrentCellChanged += onCurrentCellChanged;
 		}
 
 		public void AddRow (string name, string description, Action editCallback) {
-			grid.Rows.Add (new object [] {name, ""});
-			grid.Rows [grid.Rows.Count - 1].Cells [0].ToolTipText = description;
-			grid.Rows [grid.Rows.Count - 1].Cells [0].Style.WrapMode = DataGridViewTriState.True;
-			grid.Rows [grid.Rows.Count - 1].Cells [1].ToolTipText = description;
-			grid.Rows [grid.Rows.Count - 1].Cells [1].Style.WrapMode = DataGridViewTriState.True;
-			keys.Add (name, editCallback);
+			this.Rows.Add (new object [] {name + " - " + description, ""});
+			rows.Add (new Row (name, description, this.Rows [this.Rows.Count - 1], editCallback));
+			//this.Rows [this.Rows.Count - 1].Cells [0].ToolTipText = name + " - " + description;
+			//this.Rows [this.Rows.Count - 1].Cells [0].Style.WrapMode = DataGridViewTriState.True;
+			//this.Rows [this.Rows.Count - 1].Cells [1].ToolTipText = description;
+			//this.Rows [this.Rows.Count - 1].Cells [1].Style.WrapMode = DataGridViewTriState.True;
 		}
 
 		public void SetValue (string name, string value) {
-			GetRow (name).Cells [1].Value = value;
+			rows.Find (r => r.Name == name).RowReference.Cells [1].Value = value;
+			//rows.Find (r => r.Name == name).RowReference.Cells [1].ToolTipText = value;
 		}
 
 		private void onDisposed (object obj, EventArgs e) {
-			grid.CellDoubleClick -= onCellDoubleClick;
-			grid.CurrentCellChanged -= onCurrentCellChanged;
-			grid.KeyDown -= onKeyDown;
+			this.Disposed -= onDisposed;
+			this.CellDoubleClick -= onCellDoubleClick;
+			this.KeyDown -= onKeyDown;
+			this.CurrentCellChanged -= onCurrentCellChanged;
+		}
+
+		private void onCurrentCellChanged (object obj, EventArgs e) {
+			//using (Graphics g = CreateGraphics ()) {
+			//	Debug.WriteLine (this.CurrentCell.Value);
+			//	SizeF size = g.MeasureString (this.CurrentCell.Value as string, this.Font, this.Columns [0].Width - this.DefaultCellStyle.Padding.All * 2);
+			//	this.CurrentRow.MinimumHeight = (int) size.Height + this.DefaultCellStyle.Padding.All * 20;
+			//}
 		}
 
 		private void onCellDoubleClick (object obj, DataGridViewCellEventArgs e) {
@@ -56,37 +59,39 @@ namespace AnyGameEngineEditor {
 				return;
 			}
 			
-			foreach (KeyValuePair <string, Action> pair in keys) {
-				if ((string) grid.Rows [e.RowIndex].Cells [0].Value == pair.Key) {
-					pair.Value ();
-				}
-			}
+			rows.Find (r => r.RowReference == this.CurrentCell.OwningRow).EditCallback ();
 		}
 
 		private void onKeyDown (object obj, KeyEventArgs e) {
 			if (e.KeyCode == Keys.Enter) {
-				Action action;
-
-				if (keys.TryGetValue ((string) grid.CurrentRow.Cells [0].Value, out action)) {
-					action ();
-					grid.Focus ();
-					e.Handled = true;
-				}
+				rows.Find (r => r.RowReference == this.CurrentCell.OwningRow).EditCallback ();
+				this.Focus ();
+				e.Handled = true;
 			}
 		}
 
-		private void onCurrentCellChanged (object obj, EventArgs e) {
-			description.Text = grid.CurrentCell.ToolTipText;
-		}
+		//private DataGridViewRow GetRow (string name) {
+		//	for (int i = 0; i < this.Rows.Count; i++) {
+		//		if ((string) this.Rows [i].Cells [0].Value == name) {
+		//			return this.Rows [i];
+		//		}
+		//	}
 
-		private DataGridViewRow GetRow (string name) {
-			for (int i = 0; i < grid.Rows.Count; i++) {
-				if ((string) grid.Rows [i].Cells [0].Value == name) {
-					return grid.Rows [i];
-				}
+		//	return null;
+		//}
+
+		public class Row {
+			public string Name;
+			public string Description;
+			public DataGridViewRow RowReference;
+			public Action EditCallback;
+
+			public Row (string name, string description, DataGridViewRow row, Action editCallback) {
+				Name = name;
+				Description = description;
+				RowReference = row;
+				EditCallback = editCallback;
 			}
-
-			return null;
 		}
 	}
 }
